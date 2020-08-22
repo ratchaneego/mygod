f2530f0805d6ca6e26c1accf8dc5add2f2ddf634
Ratchanee Kumtinkaew <ratchanee.k@kbtg.tech>
2020-08-22 13:57:18 +0700

    commit

---
+++ myapi/main.go
@@ -0,0 +1,16 @@
+package main
+import (
+ "net/http"
+ 
+ "github.com/gin-gonic/gin"
+)
+func helloHandler(c *gin.Context) {
+ c.JSON(http.StatusOK, gin.H{
+ "message": "hello",
+ })
+}
+func main() {
+ r := gin.Default()
+ r.GET("/hello", helloHandler)
+ r.Run() // listen and serve on 127.0.0.0:8080
+}
